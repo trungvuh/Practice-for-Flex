40b0278c5674b0ac9ada2ab63760e65a475b049e
TV <trungvuh@gmail.com>
2017-12-17 14:14:22 -0800

    Add gif

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var totalTime = 0;
-var totalDistance = 0;
-var totalOriginal, timeOriginal;
+var newTime = 0;
+var newDistance = 0;
+var originalDistance, originalTime;
 var kmToMile = 0.621371/1000;
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: {lat: 37.166344, lng: -121.296944 }
+    center: {lat: 40.612969, lng: -96.455751 }
   });
 
   //DirectionsService will take care of setting up the route
@@ -17,11 +17,8 @@ function initMap() {
     panel: document.getElementById('right-panel'),
   });
 
-  var driverStart = 'San Francisco, CA';
-  var driverEnd = 'Shafter, CA';
-
-
-  calculateOriginal(driverStart, driverEnd, directionsService, directionsDisplay);
+  var driverStart = '825 battery st, san francisco, ca';
+  var driverEnd = '103 heath st, milpitas ca';
 
   //onClick submit will set the customer location, calculating and display new adjusted Route
   document.getElementById('submit').addEventListener('click', function() {
@@ -29,6 +26,7 @@ function initMap() {
     var customerEnd = document.getElementById('cust-end').value;
     checkDistance(driverStart, customerStart);
     checkDistance(driverEnd, customerEnd);
+    calculateOriginal(driverStart, driverEnd, directionsService);
     displayRoute(driverStart, driverEnd, customerStart, customerEnd, directionsService, directionsDisplay);
   });
 
@@ -61,7 +59,7 @@ function checkDistance(origin, checkpoint) {
   });
 }
 
-function calculateOriginal(origin, destination, service, display) {
+function calculateOriginal(origin, destination, service) {
   service.route({
     origin: origin,
     destination: destination,
@@ -69,14 +67,14 @@ function calculateOriginal(origin, destination, service, display) {
     avoidTolls: true
   }, function (response, status) {
     if (status === 'OK') {
-      display.setDirections(response);
+      // display.setDirections(response);
       var route = response.routes[0];
       var summaryPanel = document.getElementById('original-panel');
-      totalOriginal = Math.ceil((route.legs[0].distance.value * kmToMile));
-      timeOriginal = route.legs[0].duration.text;
+      originalDistance = Math.ceil((route.legs[0].distance.value * kmToMile));
+      originalTime = route.legs[0].duration.text;
       summaryPanel.innerHTML = '';
-      summaryPanel.innerHTML += '<b>Original Distance: ' + totalOriginal + ' miles <br>';
-      summaryPanel.innerHTML += '<b>Original Time: ' + timeOriginal + '<br>';
+      summaryPanel.innerHTML += '<b>Original Distance: ' + originalDistance + ' miles <br>';
+      summaryPanel.innerHTML += '<b>Original Time: ' + originalTime + '<br>';
     }
   });
 }
@@ -91,15 +89,16 @@ function displayRoute(origin, destination, customerStart, customerEnd, service, 
   }, function(response, status) {
     if (status === 'OK') {
       display.setDirections(response);
+      console.log(response);
       // calculateOriginal(origin, destination, service);
       var summaryPanel = document.getElementById('original-panel');
 
-      summaryPanel.innerHTML += '<b>New Distance: ' + totalDistance + ' miles <br>';
+      summaryPanel.innerHTML += '<b>New Distance: ' + newDistance + ' miles <br>';
       summaryPanel.innerHTML += '<b>New Time: ' +
-        Math.floor(totalTime/3600) + ' hours ' +
-        Math.floor((totalTime/60)%60) + ' minutes <br>';
+        Math.floor(newTime/3600) + ' hours ' +
+        Math.floor((newTime/60)%60) + ' minutes <br>';
 
-      var diff = (totalDistance - totalOriginal);
+      var diff = (newDistance - originalDistance);
       summaryPanel.innerHTML += '<b>Differences: ' + Math.ceil(diff) + ' miles <br>';
       summaryPanel.innerHTML += '<b>Est. Price ($.25/mile): $' + (diff * 0.25).toFixed(2) + '<br>';
       // var route = response.routes[0];
@@ -124,11 +123,11 @@ function displayRoute(origin, destination, customerStart, customerEnd, service, 
 
 function computeTotalDistance(result) {
   var myroute = result.routes[0];
-  totalTime = 0;
-  totalDistance = 0;
+  newTime = 0;
+  newDistance = 0;
   for (var i = 0; i < myroute.legs.length; i++) {
-    totalDistance += Math.ceil(myroute.legs[i].distance.value * kmToMile);
-    totalTime += myroute.legs[i].duration.value;
+    newDistance += Math.ceil(myroute.legs[i].distance.value * kmToMile);
+    newTime += myroute.legs[i].duration.value;
   }
-  document.getElementById('total').innerHTML = totalDistance + ' miles';
+  document.getElementById('total').innerHTML = newDistance + ' miles';
 }
